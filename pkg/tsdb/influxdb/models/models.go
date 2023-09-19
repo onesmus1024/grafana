1bc74e9b24f0357163e621cfa051e80ffa7a64bf
Jay Clifford <jclifford@influxdata.com>
2023-09-19 13:51:17 +0100

    updated main

---
+++ pkg/tsdb/influxdb/models/models.go
@@ -0,0 +1,64 @@
+package models
+
+import (
+	"time"
+
+	"github.com/grafana/grafana-plugin-sdk-go/data"
+)
+
+type Query struct {
+	Measurement string
+	Policy      string
+	Tags        []*Tag
+	GroupBy     []*QueryPart
+	Selects     []*Select
+	RawQuery    string
+	UseRawQuery bool
+	Alias       string
+	Interval    time.Duration
+	Tz          string
+	Limit       string
+	Slimit      string
+	OrderByTime string
+	RefID       string
+}
+
+type Tag struct {
+	Key       string
+	Operator  string
+	Value     string
+	Condition string
+}
+
+type Select []QueryPart
+
+type Response struct {
+	Results []Result
+	Error   string
+}
+
+type Result struct {
+	Series   []Row
+	Messages []*Message
+	Error    string
+}
+
+type Exemplar struct {
+	SeriesLabels map[string]string
+	Fields       []*data.Field
+	RowIdx       int
+	Value        float64
+	Timestamp    time.Time
+}
+
+type Message struct {
+	Level string `json:"level,omitempty"`
+	Text  string `json:"text,omitempty"`
+}
+
+type Row struct {
+	Name    string            `json:"name,omitempty"`
+	Tags    map[string]string `json:"tags,omitempty"`
+	Columns []string          `json:"columns,omitempty"`
+	Values  [][]any           `json:"values,omitempty"`
+}
